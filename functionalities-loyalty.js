a36c319d23217b6f5c5b712e80fc890cb4846e83
Enric Girona <enricgirona93@gmail.com>
2019-10-14 12:18:42 +0200

    loyalty

--- functionalities-loyalty.js
+++ functionalities-loyalty.js
@@ -38,9 +38,9 @@ fillStatistics(members);
 
 createTable(statisticsArray);
 
-engagement(members, "least-engaged", -1, 1);
+engagement(members, "most-loyal", -1, 1);
 
-engagement(members, "most-engaged", 1, -1);
+engagement(members, "least-loyal", 1, -1);
 
 // Function Declaration
 function fillStatistics(array) {
@@ -92,8 +92,8 @@ function fillTable(array, id, i) {
   var cell2 = document.createElement("td");
   var cell3 = document.createElement("td");
   cell1.innerHTML = array[i].first_name + " " + (array[i].middle_name || " ") + " " + array[i].last_name;
-  cell2.innerHTML = array[i].missed_votes;
-  cell3.innerHTML = array[i].missed_votes_pct + " %";
+  cell2.innerHTML = ((array[i].total_votes / 100) * array[i].votes_with_party_pct).toFixed(0);
+  cell3.innerHTML = array[i].votes_with_party_pct + " %";
   newRow.appendChild(cell1);
   newRow.appendChild(cell2);
   newRow.appendChild(cell3);
@@ -101,10 +101,10 @@ function fillTable(array, id, i) {
 
 function engagement(array, id, x, y) {
   array.sort(function(a, b) {
-    if (a.missed_votes_pct > b.missed_votes_pct) {
+    if (a.votes_with_party_pct > b.votes_with_party_pct) {
       return x;
     }
-    if (a.missed_votes_pct < b.missed_votes_pct) {
+    if (a.votes_with_party_pct < b.votes_with_party_pct) {
       return y;
     }
     return 0;
@@ -112,7 +112,7 @@ function engagement(array, id, x, y) {
   for (let i = 0; i < array.length; i++) {
     if (i < Math.round(array.length / 100) * 10) {
       fillTable(array, id, i);
-    } else if (array[i - 1].missed_votes_pct == array[i].missed_votes_pct) {
+    } else if (array[i - 1].votes_with_party_pct == array[i].votes_with_party_pct) {
       fillTable(array, id, i);
     } else {
       break;
